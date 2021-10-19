0c7f20ee92ad3013025bfc0809916d4303c66f3c
johnconnor <zkmd2012@inbox.ru>
2021-10-19 13:59:22 +0500

    1 Composition API. Setup function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    home
+    <p>Name : {{name}}</p>
+    <p>Age: {{age}}</p>
+
+    <button @click="handleClick">Click</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home',  
+  setup() {
+    let name = 'mario'
+    let age = 30
+
+    const handleClick = () => {
+      console.log('button')
+    }
+    //only returned values are available for the use in a template
+    return {name, age, handleClick}
+  },
 }
 </script>
