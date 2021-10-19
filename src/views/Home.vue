f781b553b38ae391997089eeb1677ad4480b85a0
johnconnor <zkmd2012@inbox.ru>
2021-10-19 15:37:23 +0500

    4 Composition API.  Computed values are used for search

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,36 +1,30 @@
 <template>
   <div class="home">
     <h1>Home</h1> 
-    <h2>Refs</h2> 
-    <p>{{personOne.name}}----------------{{personOne.age}}</p>
-    <button @click="updatedPersonOne">Update person one</button>
-
-    <h2>Reactive</h2>
-    <p>{{personTwo.name}}----------------{{personTwo.age}}</p>
-    <button @click="updatedPersonTwo">Update person two</button>
-
-
-
-    
+    <input type="text" v-model="search">
+    <p>Search term - {{search}}</p>
+    <div v-for="name in matchingNames" :key="name">
+      {{name}}
+    </div> 
   </div>
 </template>
 
 <script>
-import { ref, reactive } from '@vue/reactivity'
+import { ref, computed } from '@vue/reactivity'
 
 export default {
   name: 'Home',  
   setup() {
-      const personOne = ref({name: 'AAA', age: 555})
-      const personTwo = reactive({name: 'BBB', age: 500}) //reactive cannot work with primitive types data
+    const names = ref(['AAA', 'BBB', 'CCC', 'DDD', 'EEE']) 
+    
+    const search = ref('')
 
-      const updatedPersonOne = () => {
-        personOne.value.age = 101010
-      }
-       const updatedPersonTwo = () => {
-        personTwo.age = 800
-      }
-      return {personOne, personTwo, updatedPersonOne, updatedPersonTwo}
+    const matchingNames = computed(() => {
+      return names.value.filter(n => n.includes(search.value) )  
+    })
+
+
+      return {names, search, matchingNames}
     } 
 }
 </script>
