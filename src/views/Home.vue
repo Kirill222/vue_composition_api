69f2cbd769e113fb1bdd4567cccdfd66331da505
johnconnor <zkmd2012@inbox.ru>
2021-10-19 18:40:00 +0500

    5 watch vs. watchEffect. Ways how to stop watchin explayned (just put this functions into a variable and fire it whenever stop watching is needed)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,26 +5,44 @@
     <p>Search term - {{search}}</p>
     <div v-for="name in matchingNames" :key="name">
       {{name}}
-    </div> 
+    </div>
+    <button @click="handleClick">Stop watch</button> 
   </div>
 </template>
 
 <script>
 import { ref, computed } from '@vue/reactivity'
+import { watch, watchEffect } from '@vue/runtime-core'
 
 export default {
   name: 'Home',  
   setup() {
-    const names = ref(['AAA', 'BBB', 'CCC', 'DDD', 'EEE']) 
-    
+    const names = ref(['AAA', 'BBB', 'CCC', 'DDD', 'EEE'])     
     const search = ref('')
+
+    //runs each time a certain value changes
+    const stopWatch = watch(search, () => {
+      console.log("Watch function ran")
+    })
+
+    //runs initially when the component first loads
+    //but also it runs whenever any value inside it changes
+    //this is more preferable than whatch
+    const stopWatchEffect = watchEffect(() => {
+      console.log("Watch effect function ran", search.value)
+    })
+
+    const handleClick = () => {
+      stopWatch()
+      stopWatchEffect()
+    }
 
     const matchingNames = computed(() => {
       return names.value.filter(n => n.includes(search.value) )  
     })
 
 
-      return {names, search, matchingNames}
+      return {names, search, matchingNames, handleClick}
     } 
 }
 </script>
