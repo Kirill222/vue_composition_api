6dd265afba0e555ace800208320d41c992fa0f78
johnconnor <zkmd2012@inbox.ru>
2021-10-20 15:52:46 +0500

    12 Create form with styles for it, poute for Create form

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Details from '../views/Details.vue'
+import Create from '../views/Create.vue'
+
 
 
 const routes = [
@@ -14,6 +16,11 @@ const routes = [
     name: 'Details',
     component: Details,
     props: true,
+  },
+  {
+    path: '/create',
+    name: 'Create',
+    component: Create,
   }
 ]
 
